7c959102ca22b7414f7cc193ece2198f755d5c7d
Vanderlei K Silva <vanderleis47@gmail.com>
2021-01-09 18:39:00 -0300

    errors corrected

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
 import Routes from './Routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
